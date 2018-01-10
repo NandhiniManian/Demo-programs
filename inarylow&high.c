e7ae68891fcc03a0b457e26e81374be27e193396
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-01-10 23:24:34 +0530

    Create inarylow&high.c

---
+++ inarylow&high.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int a[100],b,n,i,j;
+printf("enter the number:\t");
+scanf("%d\n",&n);
+for(i=0;i<n;i++)
+{
+scanf("%d\t",&a[i]);
+}
+
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+	if(a[i]>a[j])
+	{            
+		b=a[i];
+		a[i]=a[j];
+		a[j]=b;
+		
+	}
+}
+}
+printf("%d\n%d\n",a[0],a[n-1]);}
