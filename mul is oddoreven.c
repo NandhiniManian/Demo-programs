cc77301b333018019121f90dd74b5b26c485e875
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-01-18 16:20:05 +0530

    Create mul is oddoreven.c

---
+++ mul is oddoreven.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n,m,s;
+printf("Enter the numbers:\t");
+scanf("%d%d",&n,&m);
+s=n*m;
+if(s%2==0)
+{
+printf("even");
+}
+else
+{
+	
+printf("odd");	
+}
+	
+}
