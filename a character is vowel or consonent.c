a3f5c0c82e4aee59f884d1bae94160aca4409fd0
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-01-20 00:41:36 +0530

    Create a character is vowel or consonent.c

---
+++ a character is vowel or consonent.c
@@ -0,0 +1,15 @@
+# include<stdio.h>
+void main()
+{
+char s;
+printf("enter the alphabet:");
+scanf("%c",&s);
+if(s=='a'||s=='A'||s=='e'||s=='E'||s=='i'||s=='I'||s=='o'||s=='O'||S=='u'||S=='U')
+{
+printf("is vowel");
+}
+else
+{
+printf("is a consonent");
+}
+}
