6c02769d46cb7c7b49c040060f146ddfb5d83aa3
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-01-20 00:23:04 +0530

    Create a num is (+\-\0).c

---
+++ a num is (+\-\0).c
@@ -0,0 +1,22 @@
+# include<stdio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>=0)
+{
+if(a==0)
+{
+printf("no is zero");
+}
+else
+{
+printf("no is positive");
+}
+}
+else
+{
+printf("no is negative");
+}
+}
