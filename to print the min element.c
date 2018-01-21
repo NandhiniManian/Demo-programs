efad6ccc232ffce4eac73b969ec9458a329ba7f7
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-01-20 20:02:21 -0800

    Create to print the min element.c

---
+++ to print the min element.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+void main() 
+{
+int n,a[100],i,j,b;
+printf("enter the n value:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	scanf("%d",&a[i]);
+	
+}
+for(i=0;i<n;i++)
+{
+	for(j=1;j<n;j++)
+	{
+		if(a[i]>a[j])
+		{
+			b=a[i];
+			a[i]=a[j];
+			a[j]=b;
+		}
+	}
+}printf("%d",a[0]);
+}
