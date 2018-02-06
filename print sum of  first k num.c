9c6103c8eac70734489bfecd27041317826b0a4c
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-02-06 10:15:28 +0000

    Create print sum of  first k num.c

---
+++ print sum of  first k num.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+int k,i,sum=0;
+printf("enter the k value:\n");
+scanf("%d\n",&k);
+for(i=1;i<=k;i++)
+{
+sum=sum+i;  
+}
+printf("%d\n",sum);
+    return 0;
+}
