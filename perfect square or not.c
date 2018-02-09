5f7edd68eda0083609d8cdb4994389eb10ad1e8e
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-02-09 14:26:04 +0530

    Create perfect square or not.c

---
+++ perfect square or not.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+int a,i,k;
+printf("enter the number:\n");
+scanf("%d\n",&a);
+for(i=1;i<=a;i++)
+{
+ k=i*2; 
+if(k==a)
+{
+ printf("yes") ;  
+}
+    
+}
+    return 0;
+}
