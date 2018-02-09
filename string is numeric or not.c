7f2ce4b802569948280e3f754dedb1912f23606d
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-02-09 14:08:13 +0530

    Create string is numeric or not.c

---
+++ string is numeric or not.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+char a;
+printf("enter the string:\n");
+scanf("%c\n",&a);
+if(a>='1'&&a<='9')
+{
+printf("yes");
+}
+else
+{
+printf("no");    
+}
+    return 0;
+}
