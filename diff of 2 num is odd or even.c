8fa1939e989ebf1600e468f3a54ea204dbf6fb93
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-02-13 16:08:04 +0530

    Create diff of 2 num is odd or even.c

---
+++ diff of 2 num is odd or even.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+{
+int a,b,c;
+printf("enter the numbers:\t");
+scanf("%d\n%d\n",&a,&b);
+c=a-b;
+if(c%2==0)
+{
+    printf("even");
+}
+else
+printf("odd");
+}
+
+   
