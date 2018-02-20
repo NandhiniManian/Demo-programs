c7859b981636b5a9a3a7c49a8d1d2567eec66eca
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-02-20 18:11:06 +0530

    Create print the area of the farm upto 5 decimal places .c

---
+++ print the area of the farm upto 5 decimal places .c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+void main() 
+{
+float length,breadth,n;
+printf("enter the length and breadth values:\n");
+scanf("%f\n%f\n",&length,&breadth);
+n=length*breadth;
+printf("\n%.5f",n);
+}
