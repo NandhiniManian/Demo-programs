1ab7e2682551f0e1edb8ba1384bca54e93ca192e
Nandhini M <30834130+NandhiniManian@users.noreply.github.com>
2018-03-05 20:42:51 +0530

    Update a num N find the nearest greater power of 2 .c

--- a num N find the nearest greater power of 2 .c
+++ a num N find the nearest greater power of 2 .c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-int n,c=0,x=1,i;
+int n,c=0,a=1,i;
 scanf("%d",&n);
 while(n!=1)
 {
@@ -9,7 +9,7 @@ n=n/2;
 c++;
 }
 for(i=0;i<c+1;i++)
-x=x*2;
-printf("%d",x);
+a=a*2;
+printf("%d",a);
 return 0;
 }
